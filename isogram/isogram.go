1aadfdeb7eba22c2f7a9a9e33af4a6e1d2dd4c10
Blake Ashley <blake@stelly.io>
2022-02-20 10:09:05 -0500

    Got isogram working in a much more efficent way using maps. The previous version had to iterate over the entire word no matter what. This version stops when it detects a duplicated letter.

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,15 +1,35 @@
 package isogram
 
 import (
-	"strings"
+	"unicode"
+	//  "strings"
 )
 
 func IsIsogram(word string) bool {
-	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+	//	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 	var isogramExists bool
 	if word == "" {
 		isogramExists = true
 	}
+	m := make(map[rune]bool)
+	isogramExists = true
+	for _, x := range word {
+		x = unicode.ToLower(x)
+		if unicode.IsLetter(x) {
+			if m[x] {
+				isogramExists = false
+			} else if !m[x] {
+				m[x] = true
+			} else {
+				continue
+			}
+		}
+	}
+	return isogramExists
+}
+
+/*
 	for _, x := range alphabet {
 		isogram := strings.Count(strings.ToLower(word), x)
 		if isogram > 1 {
@@ -21,3 +41,4 @@ func IsIsogram(word string) bool {
 	}
 	return isogramExists
 }
+*/
