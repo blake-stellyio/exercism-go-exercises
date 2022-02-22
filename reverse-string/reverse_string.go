ad2fcb9ead27496eb7d22c8ed402a05eaa6b24e9
Blake Ashley <56891775+blake-stellyio@users.noreply.github.com>
2022-02-22 14:34:43 -0500

    Got Reverse String to pass by cleaning up some confusing code and setting the length of a certain string.

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,26 +1,13 @@
 package reverse
 
 import (
-	"fmt"
+	"strings"
 )
 
 func Reverse(input string) string {
-	var reverseString []rune
-	for i := range input {
-		reverseChar := rune(input[len(input)-(i+1)])
-		reverseString = append(reverseString, reverseChar)
-	}
-	return fmt.Sprint(reverseString)
-}
-
-/*
-func Reverse(input string) string {
-	var reverseString []string
-	stringInRune := []rune(input)
-	for i := range stringInRune {
-		reverseChar := string(stringInRune[len(input)-(i+1)])
-		reverseString = append(reverseString, reverseChar)
+	reverseString := make([]string, len(input))
+	for i, r := range input {
+		reverseString[len(input)-i-1] = string(r)
 	}
 	return strings.Join(reverseString, "")
 }
-*/
