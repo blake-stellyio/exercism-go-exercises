c70e1bb412055b5a5e297f8094475f9de9018eef
Blake Ashley <56891775+blake-stellyio@users.noreply.github.com>
2022-02-22 18:22:06 -0500

    Started and completed the "Anagram" exercise. Started the "Word Count" exercise.

---
+++ anagram/anagram.go
@@ -0,0 +1,42 @@
+package anagram
+
+import (
+	"sort"
+	"strings"
+)
+
+func Detect(subject string, candidates []string) []string {
+
+	subjectChars := strings.Split(strings.ToLower(subject), "")
+	sort.Strings(subjectChars)
+
+	var anagams []string
+	for _, i := range candidates {
+
+		if strings.EqualFold(subject, i) {
+			continue
+		}
+
+		candidateChars := strings.Split(strings.ToLower(i), "")
+		sort.Strings(candidateChars)
+
+		if len(subjectChars) != len(candidateChars) {
+			continue
+		}
+
+		var isAnagram bool
+		for i := range subjectChars {
+			if subjectChars[i] != candidateChars[i] {
+				isAnagram = false
+				break
+			} else {
+				isAnagram = true
+			}
+		}
+		if isAnagram {
+			anagams = append(anagams, i)
+		}
+
+	}
+	return anagams
+}
