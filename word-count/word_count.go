6edb90fe908708d08ef326efd6a900f8e32fdd46
Blake Ashley <56891775+blake-stellyio@users.noreply.github.com>
2022-02-24 16:08:16 -0500

    After a few hours of work, I finally got "Word Count" to pass all the tests. Much more difficult than it should have been. I want to go back and clean up the code a bit if I can.

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -8,25 +8,69 @@ import (
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
+
 	var word []string
-	wordCount := map[string]int{}
+
+	wordCount := Frequency{
+		"": 0,
+	}
+	var wordString string
 	for _, i := range phrase {
-		// this will not handle apostrophes correctly in contractions.
-		if unicode.IsPunct(i) {
+
+		wordString = strings.ToLower(strings.Join(word, ""))
+
+		if string(i) != "," && string(i) != "'" && unicode.IsPunct(i) || unicode.IsSymbol(i) {
 			continue
 		}
-		if unicode.IsSpace(i) {
-			wordString := strings.ToLower(strings.Join(word, ""))
+
+		if unicode.IsSpace(i) || string(i) == "," {
+
 			if val, ok := wordCount[wordString]; ok {
 				wordCount[wordString] = val + 1
+				wordString = ""
+				word = nil
+				continue
 			} else {
 				wordCount[wordString] = 1
+				wordString = ""
+				word = nil
+				continue
 			}
 
 		} else {
-			word = append(word, string(i))
-		}
 
+			word = append(word, string(i))
+
+		}
 	}
+
+	wordString = strings.ToLower(strings.Join(word, ""))
+
+	if val, ok := wordCount[wordString]; ok {
+		wordCount[wordString] = val + 1
+	} else {
+		wordCount[wordString] = 1
+	}
+
+	delete(wordCount, "")
+
+	if len(phrase) == len(word) {
+		wordCount[phrase] = 1
+		return wordCount
+	}
+
+	for x := range wordCount {
+		var quoteRemoval []string
+		for _, j := range x {
+			quoteRemoval = append(quoteRemoval, string(j))
+		}
+		if quoteRemoval[0] == "'" && quoteRemoval[len(x)-1] == "'" {
+			quoteRemoval = quoteRemoval[1:(len(quoteRemoval) - 1)]
+			quoteRemovedString := strings.ToLower(strings.Join(quoteRemoval, ""))
+			delete(wordCount, x)
+			wordCount[quoteRemovedString] = wordCount[quoteRemovedString] + 1
+		}
+	}
+
 	return wordCount
 }
