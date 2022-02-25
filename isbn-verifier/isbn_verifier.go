5ec8a8632a6b5459b1cc501c11974825852b2690
Blake Ashley <56891775+blake-stellyio@users.noreply.github.com>
2022-02-25 14:18:42 -0500

    Started "ISBN Verifier"

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -1,5 +1,31 @@
 package isbn
 
+import (
+	"strconv"
+	"unicode"
+)
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	var isbnINT []int
+	for _, x := range isbn {
+		if !unicode.IsDigit(x) && string(x) != "x" || string(x) != "X" {
+			return false
+		} else if unicode.IsPunct(x) && string(x) != "-" {
+			return false
+		} else if string(x) == "-" {
+			continue
+		} else {
+			var ifT int
+			// ? not sure if this is how the formula works. I think it acts as a multipler instead.
+			if string(x) == "x" || string(x) == "X" {
+				ifT = 10
+				isbnINT = append(isbnINT, ifT)
+			} else {
+				i, _ := strconv.Atoi(string(x))
+				isbnINT = append(isbnINT, i)
+			}
+
+		}
+	}
+
 }
