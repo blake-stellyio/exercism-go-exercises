f089834605e88b76eb4a9d9db305e2c5da90481c
Blake Ashley <blake@stelly.io>
2022-03-01 18:49:11 -0500

    Completed "Armstrong Numbers"

--- armstrong-numbers/armstrong_numbers.go
+++ armstrong-numbers/armstrong_numbers.go
@@ -1,7 +1,34 @@
 package armstrong
 
-func IsNumber(n int) bool {
-	for x := range n {
+import (
+	"fmt"
+	"math"
+)
 
+func IsNumber(n int) bool {
+
+	if n == 0 {
+		return true
 	}
+
+	nString := fmt.Sprintln(n)
+	var digits []float64
+
+	for _, x := range nString {
+		if float64(x)-'0' >= 0 {
+			digits = append(digits, float64(x)-'0')
+		}
+	}
+
+	var sum int
+
+	for _, x := range digits {
+		sum += int(math.Pow(x, float64(len(digits))))
+	}
+
+	if sum == n {
+		return true
+	}
+
+	return false
 }
