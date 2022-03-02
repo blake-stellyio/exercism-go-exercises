7c98d1e33a42603ba8a499e4199d7fb63b03723e
Blake Ashley <blake@stelly.io>
2022-03-02 10:19:20 -0500

    Got the "ISBN Verifier" exercise to pass!

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -8,15 +8,12 @@ import (
 func IsValidISBN(isbn string) bool {
 	var isbnINT []int
 	for _, x := range isbn {
-		if !unicode.IsDigit(x) && string(x) != "x" || string(x) != "X" {
-			return false
-		} else if unicode.IsPunct(x) && string(x) != "-" {
+		if !unicode.IsDigit(x) && string(x) != "x" && string(x) != "X" && string(x) != "-" {
 			return false
 		} else if string(x) == "-" {
 			continue
 		} else {
 			var ifT int
-			// ? not sure if this is how the formula works. I think it acts as a multipler instead.
 			if string(x) == "x" || string(x) == "X" {
 				ifT = 10
 				isbnINT = append(isbnINT, ifT)
@@ -24,8 +21,22 @@ func IsValidISBN(isbn string) bool {
 				i, _ := strconv.Atoi(string(x))
 				isbnINT = append(isbnINT, i)
 			}
-
 		}
 	}
 
+	if len(isbnINT) != 10 {
+		return false
+	}
+
+	for i, x := range isbnINT {
+		if x == 10 {
+			if i != 9 {
+				return false
+			}
+		}
+	}
+
+	output := ((isbnINT[0]*10)+(isbnINT[1]*9)+(isbnINT[2]*8)+(isbnINT[3]*7)+(isbnINT[4]*6)+(isbnINT[5]*5)+(isbnINT[6]*4)+(isbnINT[7]*3)+(isbnINT[8]*2)+isbnINT[9])%11 == 0
+
+	return output
 }
