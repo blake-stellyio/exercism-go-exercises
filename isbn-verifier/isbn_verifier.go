4261bc24d10e13d5e8bd6a5896ecfd4775784b45
Blake Ashley <blake@stelly.io>
2022-03-04 17:46:54 -0500

    Made big improvments to my "ISBN Verifier" solution

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -1,42 +1,29 @@
 package isbn
 
 import (
-	"strconv"
 	"unicode"
 )
 
 func IsValidISBN(isbn string) bool {
-	var isbnINT []int
+	var output, counter int
+
 	for _, x := range isbn {
-		if !unicode.IsDigit(x) && string(x) != "x" && string(x) != "X" && string(x) != "-" {
+		if !unicode.IsDigit(x) && x != 'x' && x != 'X' && x != '-' {
 			return false
-		} else if string(x) == "-" {
-			continue
-		} else {
-			var ifT int
-			if string(x) == "x" || string(x) == "X" {
-				ifT = 10
-				isbnINT = append(isbnINT, ifT)
-			} else {
-				i, _ := strconv.Atoi(string(x))
-				isbnINT = append(isbnINT, i)
-			}
 		}
-	}
 
-	if len(isbnINT) != 10 {
-		return false
-	}
-
-	for i, x := range isbnINT {
-		if x == 10 {
-			if i != 9 {
+		if x != '-' {
+			if (x == 'x' || x == 'X') && counter != 9 {
 				return false
+			} else if x == 'x' || x == 'X' {
+				output += 10
+			} else {
+				i := int(x - '0')
+				output += (i * (10 - counter))
 			}
+			counter++
 		}
 	}
 
-	output := ((isbnINT[0]*10)+(isbnINT[1]*9)+(isbnINT[2]*8)+(isbnINT[3]*7)+(isbnINT[4]*6)+(isbnINT[5]*5)+(isbnINT[6]*4)+(isbnINT[7]*3)+(isbnINT[8]*2)+isbnINT[9])%11 == 0
-
-	return output
+	return output%11 == 0 && counter == 10
 }
