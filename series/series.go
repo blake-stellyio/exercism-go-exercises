0804ccae07c8b9a6e0ceffc4dca96a3ee5f684e0
Blake Ashley <blake@stelly.io>
2022-03-07 12:06:38 -0500

    Completed the "Series" exercise

--- series/series.go
+++ series/series.go
@@ -1,9 +1,38 @@
 package series
 
+import (
+	"fmt"
+	"strings"
+)
+
 func All(n int, s string) []string {
-	panic("Please implement the All function")
+	var output []string
+	for x := range s {
+		// fmt.Println(x)
+		var tempList []string
+		for i := x; i < x+n && i < len(s); i++ {
+			fmt.Println(i)
+			tempList = append(tempList, string(s[i]))
+			if len(tempList) == n {
+				output = append(output, strings.Join(tempList, ""))
+			}
+		}
+	}
+	return output
 }
 
 func UnsafeFirst(n int, s string) string {
-	panic("Please implement the UnsafeFirst function")
+	for x := range s {
+		// fmt.Println(x)
+		var tempList []string
+		for i := x; i < x+n && i < len(s); i++ {
+			fmt.Println(i)
+			tempList = append(tempList, string(s[i]))
+			if len(tempList) == n {
+				output := strings.Join(tempList, "")
+				return output
+			}
+		}
+	}
+	return "failed"
 }
