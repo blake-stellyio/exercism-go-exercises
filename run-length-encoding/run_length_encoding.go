f1a2079f924567ecf3800d5e2f352038d4e7879b
Blake Ashley <blake@stelly.io>
2022-03-07 13:54:08 -0500

    First attempt at "Run Length Encoding"

--- run-length-encoding/run_length_encoding.go
+++ run-length-encoding/run_length_encoding.go
@@ -1,8 +1,47 @@
 package encode
 
+import (
+	"fmt"
+	"strings"
+)
+
 func RunLengthEncode(input string) string {
 	var counter int
-	for _, x := range 
+	var currentRune rune
+	var encodedString []string
+	for i, x := range input {
+		if i == 0 {
+			currentRune = x
+			counter++
+			continue
+		}
+
+		if x == currentRune && i != len(input)-1 {
+			counter++
+			continue
+		}
+
+		var singleUnit string
+
+		// if i == len(input)-1 {
+		// 	singleUnit = fmt.Sprintf("%d", counter) + string(currentRune)
+		// 	encodedString = append(encodedString, singleUnit)
+
+		// }
+
+		if counter == 1 {
+			singleUnit = string(currentRune)
+		} else {
+			singleUnit = fmt.Sprintf("%d", counter) + string(currentRune)
+		}
+
+		fmt.Println(singleUnit, "SU")
+		encodedString = append(encodedString, singleUnit)
+		fmt.Println(encodedString)
+		currentRune = x
+	}
+
+	return strings.Join(encodedString, "")
 }
 
 func RunLengthDecode(input string) string {
