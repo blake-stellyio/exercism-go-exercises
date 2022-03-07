ac7c07a17ffedf1e07ec46aecfbcb803e2fb263e
Blake Ashley <blake@stelly.io>
2022-03-07 12:53:59 -0500

    Started the "Run Length Encoding" exercise

---
+++ run-length-encoding/run_length_encoding.go
@@ -0,0 +1,10 @@
+package encode
+
+func RunLengthEncode(input string) string {
+	var counter int
+	for _, x := range 
+}
+
+func RunLengthDecode(input string) string {
+	panic("Please implement the RunLengthDecode function")
+}
