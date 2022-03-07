0add695c8776476524f7236031fc6bf6784d39de
Blake Ashley <blake@stelly.io>
2022-03-07 11:22:37 -0500

    Minor update to "Prime Factors" to clean up a little code

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -2,8 +2,7 @@ package prime
 
 func Factors(n int64) []int64 {
 	var primeFactors []int64
-	var i int64
-	for i = 2; i <= n; i++ {
+	for i := int64(2); i <= n; i++ {
 		for n%i == 0 {
 			if isPrime(i) {
 				primeFactors = append(primeFactors, i)
@@ -15,8 +14,7 @@ func Factors(n int64) []int64 {
 }
 
 func isPrime(n int64) bool {
-	var i int64
-	for i = 2; i*i <= n; i++ {
+	for i := int64(2); i*i <= n; i++ {
 		if n%i == 0 {
 			return false
 		}
