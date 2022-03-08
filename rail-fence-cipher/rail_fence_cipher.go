b96c08086b39d74fa6e5d02f23e6e6b97a707681
Blake Ashley <blake@stelly.io>
2022-03-08 15:33:07 -0500

    Completed the first half of "Rail Fence Cipher"

--- rail-fence-cipher/rail_fence_cipher.go
+++ rail-fence-cipher/rail_fence_cipher.go
@@ -1,7 +1,47 @@
 package railfence
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Encode(message string, rails int) string {
-	panic("Please implement the Encode function")
+
+	railsIdent := make(map[int][]string)
+
+	// for i, x := range message {
+	// 	railsIdent[i%rails] = append(railsIdent[i%rails], string(x))
+
+	// }
+
+	// fmt.Println(railsIdent)
+
+	var counter int = 1
+	var previousCounter int
+
+	for _, x := range message {
+
+		if previousCounter > counter && counter != 1 || counter == rails {
+			railsIdent[counter] = append(railsIdent[counter], string(x))
+			previousCounter = counter
+			counter--
+		} else if previousCounter < counter && counter != rails || counter == 1 {
+			railsIdent[counter] = append(railsIdent[counter], string(x))
+			previousCounter = counter
+			counter++
+		}
+
+	}
+
+	var output string
+
+	for i := 0; i <= rails; i++ {
+		output = output + strings.Join(railsIdent[i], "")
+	}
+
+	fmt.Println(output)
+
+	return output
 }
 
 func Decode(message string, rails int) string {
