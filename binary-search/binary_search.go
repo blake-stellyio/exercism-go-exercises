eb51a8db3f67768b90addf13a948f2b8a655b7d5
Blake Ashley <blake@stelly.io>
2022-03-11 16:32:09 -0500

    Completed a first attempt at the "Binary Search" exercise

--- binary-search/binary_search.go
+++ binary-search/binary_search.go
@@ -1,5 +1,23 @@
 package binarysearch
 
 func SearchInts(list []int, key int) int {
-	panic("Please implement the SearchInts function")
+
+	if len(list) < 1 {
+		return -1
+	}
+
+	if key >= list[len(list)/2] {
+		for i := len(list) / 2; i <= len(list)-1; i++ {
+			if list[i] == key {
+				return i
+			}
+		}
+	} else {
+		for i := 0; i <= len(list)-1; i++ {
+			if list[i] == key {
+				return i
+			}
+		}
+	}
+	return -1
 }
