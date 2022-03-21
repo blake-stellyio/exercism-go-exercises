4b37fc1f5cda7e845c98941b8c0e820f68848199
Blake Ashley <blake@stelly.io>
2022-03-21 19:21:32 -0400

    Cleaned up the "Beer Song" exercise. It is harder to read, but is more compact. I think it was a reasonable trade off.

--- beer-song/beer_song.go
+++ beer-song/beer_song.go
@@ -14,19 +14,14 @@ func Song() string {
 
 func Verses(start, stop int) (string, error) {
 
-	if stop < 0 || start > 99 {
+	if stop < 0 || start > 99 || start <= stop {
 		return "", errors.New("Out of range")
-	}
-
-	if start <= stop {
-		return "", errors.New("Start must be greater than stop")
 	}
 
 	var output strings.Builder
 	for x := start; x >= stop; x-- {
 		y, _ := Verse(x)
-		z := fmt.Sprintln(y)
-		output.WriteString(z)
+		output.WriteString(fmt.Sprintln(y))
 	}
 	return output.String(), nil
 }
@@ -36,19 +31,18 @@ func Verse(n int) (string, error) {
 	if n > 99 {
 		return "", errors.New("Verse is too high")
 	}
-
-	var bottles1, bottlesFor0, bottles2, takeDown string
+	s, p, count, count2 := " bottle", " bottles", strconv.Itoa(n), strconv.Itoa(n-1)
+	bottles1, bottles2, takeDown := count+p, count2+p, "Take one down and pass it around,"
+	bottlesFor0 := bottles1
 
 	switch {
-	case n > 2:
-		bottles1, bottlesFor0, bottles2, takeDown = strconv.Itoa(n)+" bottles", strconv.Itoa(n)+" bottles", strconv.Itoa(n-1)+" bottles", "Take one down and pass it around,"
 	case n == 2:
-		bottles1, bottlesFor0, bottles2, takeDown = strconv.Itoa(n)+" bottles", strconv.Itoa(n)+" bottles", strconv.Itoa(n-1)+" bottle", "Take one down and pass it around,"
+		bottles2 = count2 + s
 	case n == 1:
-		bottles1, bottlesFor0, bottles2, takeDown = strconv.Itoa(n)+" bottle", strconv.Itoa(n)+" bottle", "no more"+" bottles", "Take it down and pass it around,"
+		bottles1, bottles2, takeDown = count+s, "no more"+p, "Take it down and pass it around,"
+		bottlesFor0 = bottles1
 	case n == 0:
-		bottles1, bottlesFor0, bottles2, takeDown = "No more"+" bottles", "no more"+" bottles", "99"+" bottles", "Go to the store and buy some more,"
-
+		bottles1, bottlesFor0, bottles2, takeDown = "No more"+p, "no more"+p, "99"+p, "Go to the store and buy some more,"
 	}
 
 	return fmt.Sprintf("%s of beer on the wall, %s of beer.\n%s %s of beer on the wall.\n", bottles1, bottlesFor0, takeDown, bottles2), nil
