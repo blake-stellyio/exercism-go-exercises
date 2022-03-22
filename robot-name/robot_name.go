9b6caabae22e2d13dc4cb6ddcff5845067d3ec3d
Blake Ashley <blake@stelly.io>
2022-03-21 21:48:55 -0400

    Made some improvements to the "robot name" exercise. It is still not passing and has poor performance.

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -1,7 +1,7 @@
 package robotname
 
 import (
-	"errors"
+	// "errors"
 	"fmt"
 	"math/rand"
 	"strconv"
@@ -11,37 +11,37 @@ import (
 type Robot string
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const capacity int = 26 * 26 * 10 * 10 * 10
+const capacity int = (26 * 26 * 10 * 10 * 10)
 
-var capacityCounter int = 0
 var robotDB map[string]bool = make(map[string]bool, capacity)
 
 func (r *Robot) Name() (string, error) {
 
-	if capacityCounter == capacity {
-		return "", errors.New("name capacity exhausted")
-	}
+	// if len(robotDB) >= capacity {
+	// 	return "", errors.New("name capacity exhausted")
+	// }
 
 	if *r != "" {
 		return string(*r), nil
 	}
 
 	var output strings.Builder
+
 	for {
 
 		for i := 1; i <= 2; i++ {
 			output.WriteByte(alphabet[rand.Intn(26)])
 		}
 
-		output.WriteString(strconv.Itoa(rand.Intn(900) + 100))
+		output.WriteString(strconv.Itoa(rand.Intn(10)) + strconv.Itoa(rand.Intn(10)) + strconv.Itoa(rand.Intn(10)))
 
 		if !robotDB[output.String()] {
 			robotDB[output.String()] = true
-			capacityCounter++
 			break
 		}
 		output.Reset()
 	}
+	fmt.Println("Made it here, ", output.String())
 	*r = Robot(output.String())
 	return output.String(), nil
 }
@@ -49,5 +49,5 @@ func (r *Robot) Name() (string, error) {
 func (r *Robot) Reset() {
 	robotDB[fmt.Sprint(r)] = false
 	*r = ""
-	r.Name()
+	// r.Name()
 }
