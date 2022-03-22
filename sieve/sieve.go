9fdc58ff5f4583f71bb3f117d8c3515ec8e3201b
Blake Ashley <blake@stelly.io>
2022-03-21 21:11:57 -0400

    Got the "Sieve" exercise to pass

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,5 +1,38 @@
 package sieve
 
 func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+
+	if limit < 2 {
+		return []int{}
+	}
+
+	var nonPrimes []int
+	var primes []int
+
+	for x := 2; x <= limit; x++ {
+
+		var listed bool = false
+
+		for _, y := range nonPrimes {
+			if y == x {
+				listed = true
+				continue
+			}
+		}
+
+		if listed == true {
+			continue
+		}
+
+		p, counter, y := x, 2, 0
+
+		for y <= limit {
+			y = counter * p
+			nonPrimes = append(nonPrimes, y)
+			counter++
+		}
+
+		primes = append(primes, x)
+	}
+	return primes
 }
