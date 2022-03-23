e94d73161c48b20ef601b41a9d9d2c5d3b75af3a
Blake Ashley <blake@stelly.io>
2022-03-23 17:35:43 -0400

    Got the "Robot Name" exercise to pass tests with a new method but it is still too slow.

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
-	"strconv"
+	// "strconv"
 	"strings"
 )
 
@@ -14,6 +14,9 @@ const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const capacity int = (26 * 26 * 10 * 10 * 10)
 
 var robotDB map[string]bool = make(map[string]bool, capacity)
+var numberCounter map[string]int = make(map[string]int, 1000)
+var namesGenerated bool = false
+var numberDB []string
 
 func (r *Robot) Name() (string, error) {
 
@@ -25,20 +28,42 @@ func (r *Robot) Name() (string, error) {
 		return "", errors.New("name capacity exhausted")
 	}
 
+	if !namesGenerated {
+		numberDB = GenerateNumbers()
+	}
+
 	var output strings.Builder
 
 	for {
+
+		var position int
+
+		if len(numberDB) > 0 {
+			position = rand.Intn(len(numberDB))
+		} else {
+			break
+		}
+
+		number := numberDB[position]
+
+		if numberCounter[number] == 26*26 {
+			numberDB[position] = numberDB[0]
+			numberDB = numberDB[1:]
+			continue
+		}
 
 		for i := 1; i <= 2; i++ {
 			output.WriteByte(alphabet[rand.Intn(26)])
 		}
 
-		output.WriteString(strconv.Itoa(rand.Intn(10)) + strconv.Itoa(rand.Intn(10)) + strconv.Itoa(rand.Intn(10)))
+		output.WriteString(number)
 
 		if !robotDB[output.String()] {
 			robotDB[output.String()] = true
+			numberCounter[number] = numberCounter[number] + 1
 			break
 		}
+
 		output.Reset()
 	}
 	fmt.Println("Made it here, ", output.String())
@@ -49,4 +74,22 @@ func (r *Robot) Name() (string, error) {
 func (r *Robot) Reset() {
 	*r = ""
 	r.Name()
+}
+
+func GenerateNumbers() []string {
+	var output []string
+	for x := 0; x < 1000; x++ {
+		if x < 10 {
+			output = append(output, fmt.Sprintf("00%d", x))
+		}
+
+		if x >= 10 && x < 100 {
+			output = append(output, fmt.Sprintf("0%d", x))
+		}
+
+		if x > 99 {
+			output = append(output, fmt.Sprint(x))
+		}
+	}
+	return output
 }
