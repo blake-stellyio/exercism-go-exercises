4752321939a19aa761ec166fed0c1a46504c2a73
Blake Ashley <blake@stelly.io>
2022-03-23 18:16:58 -0400

    Improved my solution to the "Robot Name" problem to be much, much faster

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+
 	// "strconv"
 	"strings"
 )
@@ -14,9 +15,9 @@ const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const capacity int = (26 * 26 * 10 * 10 * 10)
 
 var robotDB map[string]bool = make(map[string]bool, capacity)
-var numberCounter map[string]int = make(map[string]int, 1000)
+var numberCounter, letterCounter map[string]int = make(map[string]int, 1000), make(map[string]int, 26*26)
 var namesGenerated bool = false
-var numberDB []string
+var numberDB, letterDB []string
 
 func (r *Robot) Name() (string, error) {
 
@@ -29,45 +30,52 @@ func (r *Robot) Name() (string, error) {
 	}
 
 	if !namesGenerated {
-		numberDB = GenerateNumbers()
+		numberDB, letterDB = GenerateNames()
 		namesGenerated = true
 	}
 
 	var output strings.Builder
+	// var repeatCounter int = 0
 
 	for {
 
-		var position int
+		var numPosition, letPosition int
 
-		if len(numberDB) > 0 {
-			position = rand.Intn(len(numberDB))
+		if len(numberDB) > 0 && len(letterDB) > 0 {
+			numPosition = rand.Intn(len(numberDB))
+			letPosition = rand.Intn(len(letterDB))
 		} else {
 			break
 		}
 
-		number := numberDB[position]
+		letters, numbers := letterDB[letPosition], numberDB[numPosition]
 
-		if numberCounter[number] == 26*26 {
-			numberDB[position] = numberDB[0]
+		if numberCounter[numbers] == 26*26 {
+			numberDB[numPosition] = numberDB[0]
 			numberDB = numberDB[1:]
 			continue
 		}
 
-		for i := 1; i <= 2; i++ {
-			output.WriteByte(alphabet[rand.Intn(26)])
+		if numberCounter[letters] == 1000 {
+			letterDB[letPosition] = letterDB[0]
+			letterDB = letterDB[1:]
+			continue
 		}
 
-		output.WriteString(number)
+		output.WriteString(fmt.Sprintf("%s%s", letters, numbers))
 
 		if !robotDB[output.String()] {
 			robotDB[output.String()] = true
-			numberCounter[number] = numberCounter[number] + 1
+			numberCounter[numbers] = numberCounter[numbers] + 1
+			numberCounter[letters] = numberCounter[letters] + 1
 			break
 		}
-
+		// repeatCounter++
+		// fmt.Println(repeatCounter)
 		output.Reset()
+
 	}
-	fmt.Println("Made it here, ", output.String())
+	// fmt.Println("Made it here, ", output.String())
 	*r = Robot(output.String())
 	return output.String(), nil
 }
@@ -77,20 +85,30 @@ func (r *Robot) Reset() {
 	r.Name()
 }
 
-func GenerateNumbers() []string {
-	var output []string
+func GenerateNames() ([]string, []string) {
+
+	var outputNum, outputLet []string
+
 	for x := 0; x < 1000; x++ {
 		if x < 10 {
-			output = append(output, fmt.Sprintf("00%d", x))
+			outputNum = append(outputNum, fmt.Sprintf("00%d", x))
 		}
 
 		if x >= 10 && x < 100 {
-			output = append(output, fmt.Sprintf("0%d", x))
+			outputNum = append(outputNum, fmt.Sprintf("0%d", x))
 		}
 
 		if x > 99 {
-			output = append(output, fmt.Sprint(x))
+			outputNum = append(outputNum, fmt.Sprint(x))
 		}
 	}
-	return output
+
+	for x := 0; x < 26; x++ {
+		for i := 0; i < 26; i++ {
+			outputLet = append(outputLet, fmt.Sprintf("%c%c", alphabet[x], alphabet[i]))
+		}
+	}
+
+	return outputNum, outputLet
+
 }
